9e6dead99a2da1c7630b2ef75f78e624a506c562
Vinay <vinay.py@gmail.com>
2021-05-20 10:18:22 -0400

    First Commit

---
+++ src/main/java/com/reviewnprep/awslabs/AwslabsApplication.java
@@ -0,0 +1,13 @@
+package com.reviewnprep.awslabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwslabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwslabsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reviewnprep/awslabs/AwslabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reviewnprep.awslabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwslabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
